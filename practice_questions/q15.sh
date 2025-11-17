6f10b823754d8039eb8873f998f0e3fe755b24e8
sai-dhulipala <svd@risika.com>
2025-11-17 15:47:56 +0530

    Add new solution q15

--- practice_questions/q15.sh
+++ practice_questions/q15.sh
@@ -3,4 +3,10 @@
 for SERVICE in sshd firewalld NetworkManager
 do
     systemctl is-active --quiet $SERVICE
+    if [ $? -eq 0 ]
+    then
+        echo "${SERVICE} is active"
+    else
+        echo "${SERVICE} is inactive"
+    fi
 done
