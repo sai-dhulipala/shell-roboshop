9f372851d1a7e9761b3d9e25ae38383d5b09a459
sai-dhulipala <svd@risika.com>
2025-11-19 22:59:09 +0530

    Add dispatch service script and configuration for application setup

--- backend/dispatch.sh
+++ backend/dispatch.sh
@@ -38,19 +38,11 @@ echo "Script started at: $(date)" | tee -a $LOG_FILE
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 UTILS_DIR="$SCRIPT_DIR/../utils"
 
-# Step 1: Disable NodeJS
-dnf module disable nodejs -y &>> $LOG_FILE
-echo -e "Disabling default NodeJS ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
+# Step 1: Install GoLang
+dnf install golang -y &>>$LOG_FILE
+echo -e "Installing GoLang ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
-# Step 2: Enable NoteJS:20
-dnf module enable nodejs:20 -y &>> $LOG_FILE
-echo -e "Enabling NodeJS:20 ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
-
-# Step 3: Install NodeJS
-dnf install nodejs -y &>> $LOG_FILE
-echo -e "Installing NodeJS ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
-
-# Step 4: Add application user 'roboshop'
+# Step 2: Add application user 'roboshop'
 if ! id roboshop &> /dev/null
 then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>> $LOG_FILE
@@ -60,40 +52,42 @@ else
     echo -e "Adding application user 'roboshop' ... ${Y}SKIPPING${N}" | tee -a $LOG_FILE
 fi
 
-# Step 5: Create directory '/app'
+# Step 3: Create directory '/app'
 mkdir -p /app &>> $LOG_FILE
 echo -e "Creating '/app' directory ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
-# Step 6: Download source code to tmp
-curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart-v3.zip &>> $LOG_FILE
+# Step 4: Download source code to tmp
+curl -L -o /tmp/dispatch.zip https://roboshop-artifacts.s3.amazonaws.com/dispatch-v3.zip &>> $LOG_FILE
 echo -e "Downloading source code ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
-# Step 7: Extract source code
+# Step 5: Extract source code
 cd /app &>> $LOG_FILE
 
 # We are doing this to ensure idempotency,
 # meaning even if we run the script twice, it should be working fine
 rm -rf /app/* &>> $LOG_FILE
-unzip /tmp/cart.zip &>> $LOG_FILE
+unzip /tmp/dispatch.zip &>> $LOG_FILE
 echo -e "Extracting source code ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
-# Step 8: Install dependencies
-npm install &>> $LOG_FILE
+# Step 6: Install dependencies
+go mod init dispatch &>> $LOG_FILE
+go get &>> $LOG_FILE
+go build &>> $LOG_FILE
 echo -e "Installing dependencies ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
-# Step 9: Setup SystemD Cart Service
-cp $UTILS_DIR/cart.service /etc/systemd/system/cart.service &>> $LOG_FILE
-echo -e "Setting up SystemD Cart Service ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
+# Step 7: Setup SystemD Dispatch Service
+cp $UTILS_DIR/dispatch.service /etc/systemd/system/dispatch.service &>> $LOG_FILE
+echo -e "Setting up SystemD Dispatch Service ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
-# Step 10: Reload SystemD
+# Step 8: Reload SystemD
 systemctl daemon-reload &>> $LOG_FILE
 echo -e "Reload SystemD ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
-# Step 11: Enable and Start Service
-systemctl enable cart &>> $LOG_FILE
-echo -e "Enabling cart ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
+# Step 9: Enable and Start Service
+systemctl enable dispatch &>> $LOG_FILE
+echo -e "Enabling dispatch ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
-systemctl start cart &>> $LOG_FILE
-echo -e "Starting cart ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
+systemctl start dispatch &>> $LOG_FILE
+echo -e "Starting dispatch ... ${G}SUCCESS${N}" | tee -a $LOG_FILE
 
 echo "Script ended at: $(date)" | tee -a $LOG_FILE
